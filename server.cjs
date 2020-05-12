cfbe43f51ea648ccad11b20a1b0d772065ec9ac7
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-12 09:35:17 +0200

    improved binary help and messages

--- server.cjs
+++ server.cjs
@@ -6,11 +6,13 @@ const {createServer} = require('http');
 const {cpus, networkInterfaces} = require('os');
 const {join, resolve} = require('path');
 
+const {error, log, warn} = require('essential-md');
+
 const cdn = require('./cjs/index.js');
 
 const {fork, isMaster} = cluster;
 
-let port = 0;
+let port = 8080;
 let cacheTimeout = 300000;
 let clusters = 0;
 let serve = '.';
@@ -86,39 +88,45 @@ for (let
 }
 
 if (help || (notServing && isServing)) {
-  console.log('');
-  console.log(`\x1b[1mucdn --source ./path/\x1b[0m`);
-  console.log(`  --cluster X        \x1b[2m# number of forks, default 0\x1b[0m`);
-  console.log(`  --cache /tmp       \x1b[2m# CDN cache path, default /tmp/ucdn\x1b[0m`);
-  console.log(`  --cache-timeout X  \x1b[2m# cache expiration in ms, default 300000\x1b[0m`);
-  console.log(`  --max-width X      \x1b[2m# max images width in pixels\x1b[0m`);
-  console.log(`  --max-height X     \x1b[2m# max images height in pixels\x1b[0m`);
-  console.log(`  --port XXXX        \x1b[2m# port to use, default 0 (any available port)\x1b[0m`);
-  console.log(`  --serve /path      \x1b[2m# serve a CDN ready path without any runtime\x1b[0m`);
-  console.log(`  --source ./        \x1b[2m# path to serve as CDN, default current folder\x1b[0m`);
-  console.log(`  --with-preview     \x1b[2m# enables *.preview.jpeg images\x1b[0m`);
-  console.log(`  --with-source-map  \x1b[2m# enables source maps\x1b[0m`);
-  console.log(`  --no-minify        \x1b[2m# do not minify sources\x1b[0m`);
-  console.log('');
-  console.log(`\x1b[1maliases\x1b[0m`);
-  console.log(`  --fork X           \x1b[2m# alias for --cluster\x1b[0m`);
-  console.log(`  --dest /tmp        \x1b[2m# alias for --cache\x1b[0m`);
-  console.log('');
+  log(`
+  # micro cdn v${require(join(__dirname, 'package.json')).version}
+  -https://github.com/WebReflection/ucdn-
+
+  *ucdn --source ./path/*
+    \`--cluster(s) X\`     -# number of forks, default 0-
+    \`--source ./\`        -# path to serve as CDN, default current folder-
+    \`--dest /tmp\`        -# CDN cache path, default /tmp/ucdn-
+    \`--cache-timeout X\`  -# cache expiration in ms, default 300000-
+    \`--port XXXX\`        -# port to use, default 0 (any available port)-
+    \`--serve /path\`      -# serve a CDN ready path without any runtime-
+
+  *ucompress* options
+    \`--max-width X\`      -# max images width in pixels-
+    \`--max-height X\`     -# max images height in pixels-
+    \`--with-preview\`     -# enables *.preview.jpeg images-
+    \`--with-source-map\`  -# enables source maps-
+    \`--no-minify\`        -# do not minify sources-
+
+  *aliases*
+    \`--fork(s) X\`        -# alias for \`--cluster(s)\`-
+    \`--cache /tmp\`       -# alias for \`--dest\`-
+  `);
 }
 else if (isMaster && 0 < clusters) {
   let forks = clusters;
   let toBeGreeted = true;
-  const onGreetings = port => {
+  const onGreetings = usedPort => {
     if (toBeGreeted) {
       toBeGreeted = !toBeGreeted;
-      greetings(port);
+      greetings(usedPort);
     }
   };
   while (forks--)
     fork().on('message', onGreetings);
   cluster.on('exit', ({process}, code, signal) => {
-    console.warn(`Worker ${process.pid} died with code ${code} and signal ${signal}`);
-    fork();
+    warn(`Worker *${process.pid}* died with code *${code}* and signal *${signal}*`);
+    if (!(code == 1 && !signal))
+      fork();
   });
 }
 else {
@@ -191,32 +199,42 @@ else {
       }
     })
   ;
-  createServer(($, _) => { handler($, _, next); }).listen(port, greetings);
+  createServer(($, _) => { handler($, _, next); })
+    .on('error', function() {
+      if (port == 8080)
+        this.listen(0);
+      else {
+        error(`  port *${port}* is unavailable`);
+        process.exit(1);
+      }
+    })
+    .listen(port, greetings);
 }
 
-function greetings(port = this.address().port) {
+function greetings(newPort = this.address().port) {
   if (isMaster) {
-    const checks = `\x1b[2m(checked each ${(cacheTimeout / 60000) >>> 0} min)\x1b[0m`;
-    console.log('');
-    console.log(`  \x1b[1mucdn\x1b[0m \x1b[2m${clusters ? `(${clusters} forks)` : ''}\x1b[0m`);
+    const checks = `(checked each ${(cacheTimeout / 60000) >>> 0} min)`;
+    log(`\n  *ucdn* ${clusters ? `-(${clusters} forks)-` : ''}`);
+    if (newPort != port)
+      warn(`  port *${port}* not available, using *${newPort}* instead`);
     if (isServing) {
-      console.log(`  \x1b[2mserving:\x1b[0m ${resolve(process.cwd(), serve)} ${checks}`);
+      log(`  -serving:- ${resolve(process.cwd(), serve)} -${checks}-`);
     }
     else {
-      console.log(`  \x1b[2msource: \x1b[0m ${resolve(process.cwd(), source)} ${checks}`);
-      console.log(`  \x1b[2mcache:   ${dest ? resolve(process.cwd(), dest) : '/tmp/ucdn'}\x1b[0m`);
+      log(`  -source:-  ${resolve(process.cwd(), source)} -${checks}-`);
+      log(`  -cache:-   -${dest ? resolve(process.cwd(), dest) : '/tmp/ucdn'}-`);
     }
-    console.log(`  \x1b[2mvisit:  \x1b[0m \x1b[1mhttp://localhost${port == 80 ? '' : `:${port}`}/\x1b[0m`);
+    log(`  -visit:-   *http://localhost${newPort == 80 ? '' : `:${newPort}`}/*`);
     const interfaces = networkInterfaces();
     Object.keys(interfaces).forEach(key => {
       interfaces[key].forEach(iFace => {
         const {address, family} = iFace;
         if (family === 'IPv4' && address !== '127.0.0.1')
-          console.log(`           \x1b[1mhttp://${address}${port == 80 ? '' : `:${port}`}/\x1b[0m`);
+          log(`           *http://${address}${newPort == 80 ? '' : `:${newPort}`}/*`);
       });
     });
-    console.log('');
+    log('');
   }
   else
-    process.send(port);
+    process.send(newPort);
 }
